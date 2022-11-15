a7b1cb6e118b41b5b798676183e55c555b054176
Divya-Bodake <divyabodake25@gmail.com>
2022-11-15 12:05:14 +0530

    [Divya]Add:Using java Compare Methods

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -7,12 +7,12 @@ namespace LineComparisonComputation
         static void Main(string[] args)
         {
             // Console.WriteLine("welcome to line comparision Problem");
-            Comparison compare1 = new Comparison(3, 5, 8, 7);//parameterised constructor
+            Comparison compare1 = new Comparison(3, 5, 8, 7);
             double line1 = compare1.calculate();
-            Comparison compare2 = new Comparison(1, 5, 8, 7);
+            Comparison compare2 = new Comparison(3, 5, 8, 7);
             double line2 = compare2.calculate();
 
-            if (line1.Equals(line2))
+            if (line1.CompareTo(line2) == 0)
             {
                 Console.WriteLine("line1 and line2 are equal");
             }
