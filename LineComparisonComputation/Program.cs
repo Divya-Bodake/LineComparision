8dd5e686f359eee6193e554ac56ec57cc85715c8
Divya-Bodake <divyabodake25@gmail.com>
2022-11-15 11:31:01 +0530

    [Divya]Add:Calculate length of line

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Line Comparision Computation Program");
+            Comparison comparison = new Comparison();
+            comparison.value(2, 4, 6, 8);
+            comparison.calculate();
         }
     }
 }
